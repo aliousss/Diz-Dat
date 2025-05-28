dbbfe46c293fd92e795da22a3292d53807b6b55c
Ajax <aliemaji01@gmail.com>
2025-05-28 23:04:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+﻿function toggleAdminPanel() {
+  document.getElementById('adminPanel').classList.toggle('hidden');
+}
+
+function openLoginModal() {
+  document.getElementById('adminPanel').classList.add('hidden');
+  document.getElementById('loginModal').classList.remove('hidden');
+}
+
+function closeModal(modalId) {
+  document.getElementById(modalId).classList.add('hidden');
+}
+
+document.getElementById('contactForm')?.addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert("Thank you for contacting Diz&Dat!");
+});
